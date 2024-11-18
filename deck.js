b108737f9de6f6b1c323bf4f90a93ba0330490ae
Gil <gil.a.sherman@gmail.com>
2024-11-18 18:07:05 +0000

    added-deckjs-deck-builder
    new deck built, and called into main script.js

--- deck.js
+++ deck.js
@@ -1,4 +1,30 @@
-class Deck {
-  constructor() {
-    this.cards = [];
+// This script is used to create a deck of cards
+
+const SUITS = ['♥', '♦', '♣', '♠'];
+const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+// Deck class to represent a deck of cards
+// Built to account for a deck of any number of cards
+export default class Deck {
+  constructor(cards = createDeck()) {
+    this.cards = cards;
+  }
+}
+
+// Card class to represent an individual card
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
+
+// Function to create a deck of cards
+// Returns an array of Card instances
+function createDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value);
+    });
+  });
 }
--- script.js
+++ script.js
@@ -1,40 +1,4 @@
-// Card class to represent individual cards
-class Card {
-    // Initialising cards with suit and rank
-    constructor(suit, rank) {
-        this.suit = suit;
-        this.rank = rank;
-    }
+import Deck from './deck.js';
 
-    getValue() {
-        // Create a mapping of card ranks to their numeric values
-        // (Face cards have higher values: Jack = 11, Queen = 12, King = 13
-        // Ace is highest with value of 14
-        const values = {
-            '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
-            '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14
-        };
-        return values[this.rank];
-    }
-}
-
-// Game class
-class Game {
-    // Game implementation
-}
-
-// UI Functions
-function updateUI(playerCard, computerCard, result) {
-    // UI update logic
-}
-
-// Event Listeners
-document.addEventListener('DOMContentLoaded', () => {
-    // Game initialization and event handling
-});
-
-//account for deck running out of cards
-
-//account for reset function
-
-//account for new game button
+const deck = new Deck();
+console.log(deck.cards);
