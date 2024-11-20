7ac7051176f497606405d6200fbd5fab835997ed
Gil <gil.a.sherman@gmail.com>
2024-11-20 17:02:40 +0000

    animation-config-&-CSS-animations

---
+++ src/js/config/animationConfig.js
@@ -0,0 +1,16 @@
+// Animation configurations
+export const ANIMATIONS = {
+  duration: {
+      card: 500,    // 500ms for card animations
+      score: 300,   // 300ms for score updates
+      message: 400  // 400ms for message displays
+  },
+  classes: {
+      card: {
+          flip: 'card-flip-animation',
+          deal: 'card-deal-animation'
+      },
+      score: 'score-pulse-animation',
+      message: 'message-fade-animation'
+  }
+};
